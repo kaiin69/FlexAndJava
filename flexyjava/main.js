2b6d4a87322274a647fa5360b09498655d872dcc
Andres Muñoz <126924107+kaiin69@users.noreply.github.com>
2023-03-23 15:29:22 -0500

    Add files via upload

---
+++ flexyjava/main.js
@@ -0,0 +1,24 @@
+var usuario = prompt("ingrese su usuario"),
+      clave=prompt("ingrese su clave");
+
+ let  clavelet="1234",
+      usuariolet="andres";
+
+
+    if(usuario  ==  usuariolet && clave ==clavelet) {
+        alert("puede ver las cards");
+        
+        }else{
+            alert ("no puede ver las cards");
+        
+        }
+
+        if(usuario != usuariolet && clave != clavelet){
+            alert("no puede ver las cards")
+
+        }
+
+        
+        
+//    function ingresoexito(usuariolet,clavelet)
+//         var ingreso= usuariolet + clavelet;
